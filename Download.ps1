4d34708aadfbfaccf8779938b76b930d2ef0621d
Bright1192 <87061166+Bright1192@users.noreply.github.com>
2023-08-31 18:26:28 +0800

    CheckUpdate

--- Download.ps1
+++ Download.ps1
@@ -2,8 +2,8 @@ param(
     [string]$Authorization
 )
 
-Remove-Item -Path "Localize" -Recurse
-Copy-Item -Path LocalizeLimbusCompany/Localize . -Force -Recurse
+#Remove-Item -Path "Localize" -Recurse
+#Copy-Item -Path LocalizeLimbusCompany/Localize . -Force -Recurse
 
 $ErrorFile = "./Error.txt"
 if ((Test-Path -Path $ErrorFile)) {
@@ -29,7 +29,6 @@ if (Test-Path -Path "./Error.txt") {
           echo "error=$error" >> $env:GITHUB_OUTPUT
           echo "has_error=true" >> $env:GITHUB_OUTPUT
         } else {
-          echo "has_error=false" >> $env:GITHUB_OUTPUT
           git config --global user.name 'github-actions[bot]'
           git config --global user.email 'github-actions[bot]@users.noreply.github.com'
           git add Localize/*
