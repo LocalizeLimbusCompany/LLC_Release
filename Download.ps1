81629526aa62176050d632c17a04110bcaed49ee
Bright1192 <87061166+Bright1192@users.noreply.github.com>
2023-11-18 02:06:29 +0800

    Init

---
+++ Download.ps1
@@ -0,0 +1,38 @@
+param(
+    [string]$Authorization
+)
+Set-Location "LocalizeLimbusCompany/Localize"
+
+$ErrorFile = "./Error.txt"
+if ((Test-Path -Path $ErrorFile)) {
+Remove-Item $ErrorFile
+	}
+else {
+$url = "https://paratranz.cn/api/projects/6860/artifacts/download"
+$headers = @{
+    "Authorization" = "$Authorization"
+    "accept" = "*/*"
+	}
+$response = Invoke-WebRequest -Uri $url -Headers $headers -Method Get
+[IO.File]::WriteAllBytes("./LocalizeLimbusCompany/Localize/test.zip", $response.Content)
+
+# Unzip the file to the current directory
+Expand-Archive -Path "test.zip" -DestinationPath "." -Force
+	}
+
+Start-Process "lib/LLC_To_Paratranz.exe" -Wait
+
+if (Test-Path -Path "./Error.txt") {
+          $error = Get-Content ./Error.txt
+          echo "error=$error" >> $env:GITHUB_OUTPUT
+          echo "has_error=true" >> $env:GITHUB_OUTPUT
+        } else {
+          git config --global user.name 'github-actions[bot]'
+          git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+          git add CN/*
+          $commitMessage = $(Get-Date -Format "MM-dd")+" AutoUpdate"
+          git commit -m $commitMessage
+          git push https://github-actions[bot]:${ secrets.GH_TOKEN }@github.com/LocalizeLimbusCompany/LLC_Release
+          Set-Location "../"
+          ./build.ps1 Dev
+        }
