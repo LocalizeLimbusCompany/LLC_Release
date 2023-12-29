9b804e57d71802adc397921f8684160a94b0b371
曾小皮-ZengXiaoPi <102937102+ZengXiaoPi@users.noreply.github.com>
2023-12-29 20:34:45 +0800

    Change API website

--- Download.ps1
+++ Download.ps1
@@ -39,5 +39,5 @@ if (Test-Path -Path "./Error.txt") {
           $upload_headers = @{
               "Authorization" = "$ServerAuthorization"
           }
-          curl -X POST http://45.158.169.136:4512/upload -F "file=@./Release/LimbusLocalize_BIE_Dev.7z" -H "Authorization: $ServerAuthorization"
+          curl -X POST https://api.zeroasso.top/upload -F "file=@./Release/LimbusLocalize_BIE_Dev.7z" -H "Authorization: $ServerAuthorization"
         }
